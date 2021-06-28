8adf70af4d1fa869df2910073fa4e59fb04a8609
Rawnaqaburumman <engrawnaqaburummn@gmail.com>
2021-06-28 21:12:29 +0300

    edit

--- Js/app.js
+++ Js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 
-const hours= [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7];
-let Seattle = {
+const hours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7];
+let num =[];
+let randomcustomerhour=[];
+/*let Seattle = {
     mincust: 23,
     maxcust: 65,
     avg: 6.3,
@@ -161,124 +163,210 @@ let Lima = {
 //The end of Lima object
 Lima.ranodomcust();
 Lima.cookiesnumber();
-console.log(Lima);
+console.log(Lima);*/
 
 //--------------------------------DOC--------------------------------------------------------------------------------------------------------------------
 
-let parent = document.getElementById('begin');
-let header2 = document.createElement('h2');
-parent.appendChild(header2);
-header2.textContent = 'Seattle ';
 
-let unOrderedList_1 = document.createElement('ul');
-parent.appendChild(unOrderedList_1);
+function Site (mincust, maxcust, avg) {
+    this.mincust = mincust
+    this.maxcust = maxcust
+    this.avg = avg
 
-for (let i = 0; i < Seattle.cookies.length; i++) {
-    let listItem = document.createElement('li');
-    unOrderedList_1.appendChild(listItem);
-    listItem.textContent = `${Seattle.hours[i]} pm ${Seattle.cookies[i]}`
-
-
-}
-let add = function(arr) {
-    return arr.reduce((a, b) => a + b, 0);
-};
- //var arr = Seattle.cookies;
- let sum1 = add(Seattle.cookies);
-
-let paragrapgh = document.createElement('p');
-parent.appendChild(paragrapgh);
-paragrapgh.textContent = ` The sum is: ${sum1}`;
-
-
-
-//--------------------------------------------------------------------------------
-
-let header2_2 = document.createElement('h2');
-parent.appendChild(header2_2);
-header2_2.textContent = 'Tokyo';
-
-let unOrderedList_2 = document.createElement('ul');
-parent.appendChild(unOrderedList_2);
-for (let i = 0; i < Tokyo.cookies.length; i++) {
-    let listItem = document.createElement('li');
-    unOrderedList_2.appendChild(listItem);
-    listItem.textContent = `${Tokyo.hours[i]} pm ${Tokyo.cookies[i]}`
-}
-
-
-let sum2 = add(Tokyo.cookies);
-
-let paragrapgh_2 = document.createElement('p');
-parent.appendChild(paragrapgh_2);
-paragrapgh_2.textContent = ` The sum is: ${sum2}`;
-
-
-
-
-//-------------------------------------------------------------------------------------
-
-let header2_3 = document.createElement('h2');
-parent.appendChild(header2_3);
-header2_3.textContent = 'Dubai';
-
-
-let unOrderedList_3 = document.createElement('ul');
-parent.appendChild(unOrderedList_3);
-for (let i = 0; i < Dubai.cookies.length; i++) {
-    let listItem = document.createElement('li');
-    unOrderedList_3.appendChild(listItem);
-    listItem.textContent = `${Dubai.hours[i]} pm ${Dubai.cookies[i]}`
-}
-
-
-let sum3 = add(Dubai.cookies);
-
-let paragrapgh_3 = document.createElement('p');
-parent.appendChild(paragrapgh_3);
-paragrapgh_3.textContent = ` The sum is: ${sum3}`;
-
-
-//----------------------------------------------------------------------------------------
-
-
-let header2_4 = document.createElement('h2');
-parent.appendChild(header2_4);
-header2_4.textContent = 'Lima';
-
-let unOrderedList_4 = document.createElement('ul');
-parent.appendChild(unOrderedList_4);
-for (let i = 0; i < Lima.cookies.length; i++) {
-    let listItem = document.createElement('li');
-    unOrderedList_4.appendChild(listItem);
-    listItem.textContent = `${Lima.hours[i]} pm ${Lima.cookies[i]}`
-}
-
-let sum4 = add(Lima.cookies);
-
-let paragrapgh_4 = document.createElement('p');
-parent.appendChild(paragrapgh_4);
-paragrapgh_4.textContent = ` The sum is: ${sum4}`;
-
-
-//--------------------------------------------------------------------------------------
-let header2_5 = document.createElement('h2');
-parent.appendChild(header2_5);
-header2_5.textContent = 'Paris';
-
-let unOrderedList_5 = document.createElement('ul');
-parent.appendChild(unOrderedList_5);
-for (let i = 0; i < Paris.cookies.length; i++) {
-    let listItem = document.createElement('li');
-    unOrderedList_5.appendChild(listItem);
-    listItem.textContent = `${Paris.hours[i]} pm ${Paris.cookies[i]}`;
+    this.randomcust1  =[];
+    this.cookiesnumber1 =[];
 
 }
 
-let sum5 = add(Paris.cookies);
+function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
 
-let paragrapgh_5 = document.createElement('p');
-parent.appendChild(paragrapgh_5);
-paragrapgh_5.textContent = ` The sum is: ${sum5}`;
+Site.prototype.randomcust = function () {
+    for (let j = 0; j < 14; j++) {
+        this.randomcust1.push(getRandomArbitrary(this.mincust,this.maxcust))
+
+    }
+}
+    Site.prototype.cookiesnumber = function () {
+  
+            for (let i = 0; i < 14; i++) {
+                let num = Math.floor(this.randomcust1[i] * this.avg)
+
+                this.cookiesnumber1.push(num);
+
+            }
+    }
+let Seattle = new Site (23, 65, 6.3);
+let Paris = new Site (20,38,2.3)
+Seattle.randomcust ();
+Seattle.cookiesnumber();
+console.log(Seattle);
 
 
+
+  let parent = document.getElementById('begin');
+    let header2 = document.createElement('h2');
+    parent.appendChild(header2);
+    header2.textContent = 'Seattle ';
+
+    let unOrderedList_1 = document.createElement('ul');
+    parent.appendChild(unOrderedList_1);
+
+    for (let i = 0; i < Seattle.cookiesnumber1.length; i++) {
+        let listItem = document.createElement('li');
+        unOrderedList_1.appendChild(listItem);
+        listItem.textContent = `${hours[i]} pm ${Seattle.cookiesnumber1[i]}`
+
+
+    }
+console.log(Seattle.cookiesnumber)
+   let add = function (arr) {
+        return arr.reduce((a, b) => a + b, 0);
+    };
+    //var arr = Seattle.cookies;
+    let sum1 = add(Seattle.cookiesnumber1);
+
+    let paragrapgh = document.createElement('p');
+    parent.appendChild(paragrapgh);
+    paragrapgh.textContent = ` The sum is: ${sum1}`;
+
+
+
+    //--------------------------------------------------------------------------------
+
+
+
+    let Tokyo = new Site (3, 24, 1.2);
+
+Tokyo.randomcust ();
+Tokyo.cookiesnumber();
+console.log(Tokyo);
+
+    let header2_2 = document.createElement('h2');
+    parent.appendChild(header2_2);
+    header2_2.textContent = 'Tokyo';
+
+    let unOrderedList_2 = document.createElement('ul');
+    parent.appendChild(unOrderedList_2);
+    for (let i = 0; i < Tokyo.cookiesnumber1.length; i++) {
+        let listItem = document.createElement('li');
+        unOrderedList_2.appendChild(listItem);
+        listItem.textContent = `${hours[i]} pm ${Tokyo.cookiesnumber1[i]}`
+    }
+
+
+    let sum2 = add(Tokyo.cookiesnumber1);
+
+    let paragrapgh_2 = document.createElement('p');
+    parent.appendChild(paragrapgh_2);
+    paragrapgh_2.textContent = ` The sum is: ${sum2}`;
+
+
+
+
+    //-------------------------------------------------------------------------------------
+
+
+
+
+let Paris = new Site (20,38,2.3)
+Seattle.randomcust ();
+Seattle.cookiesnumber();
+console.log(Seattle);
+
+    let header2_3 = document.createElement('h2');
+    parent.appendChild(header2_3);
+    header2_3.textContent = 'Dubai';
+
+
+    let unOrderedList_3 = document.createElement('ul');
+    parent.appendChild(unOrderedList_3);
+    for (let i = 0; i < Dubai.cookies.length; i++) {
+        let listItem = document.createElement('li');
+        unOrderedList_3.appendChild(listItem);
+        listItem.textContent = `${Dubai.hours[i]} pm ${Dubai.cookies[i]}`
+    }
+
+
+    let sum3 = add(Dubai.cookies);
+
+    let paragrapgh_3 = document.createElement('p');
+    parent.appendChild(paragrapgh_3);
+    paragrapgh_3.textContent = ` The sum is: ${sum3}`;
+
+
+    //----------------------------------------------------------Table code ------------------------------
+
+   // let parent = document.getElementById('begin');
+    let header2 = document.createElement('h2');
+    parent.appendChild(header2);
+
+    header2.textContent = 'The table';
+
+      // create table
+      let tableEl = document.createElement('table');
+      // append
+      parent.appendChild(tableEl);
+      
+      // creating the tr in the table
+      let headingRow = document.createElement('tr');
+      tableEl.appendChild(headingRow);
+      
+      let nameEle = document.createElement('th');
+      headingRow.appendChild(nameEle);
+      nameEle.textContent = '6 pm';
+      let ageEle = document.createElement('th');
+headingRow.appendChild(ageEle);
+ageEle.textContent = '7 pm';
+
+let weightEle = document.createElement('th');
+headingRow.appendChild(weightEle);
+weightEle.textContent = '8 pm';
+
+
+
+    /*let header2_4 = document.createElement('h2');
+    parent.appendChild(header2_4);
+    header2_4.textContent = 'Lima';
+
+    let unOrderedList_4 = document.createElement('ul');
+    parent.appendChild(unOrderedList_4);
+    for (let i = 0; i < Lima.cookies.length; i++) {
+        let listItem = document.createElement('li');
+        unOrderedList_4.appendChild(listItem);
+        listItem.textContent = `${Lima.hours[i]} pm ${Lima.cookies[i]}`
+    }
+
+    let sum4 = add(Lima.cookies);
+
+    let paragrapgh_4 = document.createElement('p');
+    parent.appendChild(paragrapgh_4);
+    paragrapgh_4.textContent = ` The sum is: ${sum4}`;
+
+
+    //--------------------------------------------------------------------------------------
+    let header2_5 = document.createElement('h2');
+    parent.appendChild(header2_5);
+    header2_5.textContent = 'Paris';
+
+    let unOrderedList_5 = document.createElement('ul');
+    parent.appendChild(unOrderedList_5);
+    for (let i = 0; i < Paris.cookies.length; i++) {
+        let listItem = document.createElement('li');
+        unOrderedList_5.appendChild(listItem);
+        listItem.textContent = `${Paris.hours[i]} pm ${Paris.cookies[i]}`;
+
+    }
+
+    let sum5 = add(Paris.cookies);
+
+    let paragrapgh_5 = document.createElement('p');
+    parent.appendChild(paragrapgh_5);
+    paragrapgh_5.textContent = ` The sum is: ${sum5}`;*/
+
+
+   
+    
